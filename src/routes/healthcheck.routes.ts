c6530b1c7f6d316d7220e77ff24ee50676a0dd64
Arpan <64348757+MeMasterMind@users.noreply.github.com>
2025-03-22 21:57:05 +0530

    a

--- src/routes/healthcheck.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { healthcheck } from "../controllers/healthcheck.controller";
-
-const router: Router = Router()
-
-router.route("/").get(healthcheck);
-
-export default router
