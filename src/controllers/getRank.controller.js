492cf8d370ad4960fc05c1dee88dc88be892b87a
Ayush rawat <165577927+Ayu-Rawat@users.noreply.github.com>
2025-03-27 20:52:58 +0530

    feat: update database connection to db03 and enhance getRank controller to accept percentile and year parameters

--- src/controllers/getRank.controller.js
+++ src/controllers/getRank.controller.js
@@ -1,9 +1,12 @@
 import {ApiError} from '../utils/ApiError.js';
 import {ApiResponse} from '../utils/ApiResponse.js';
 import {asyncHandler} from '../utils/asyncHandler.js';
-import sql from '../db/db01.js';
+import sql from '../db/db03.js';
 
 const getRank = asyncHandler(async (req, res) => {
+    const { percentile , year} = req.body;
+
+    const query = `SELECT * FROM data_${year} WHERE `;
 });
 
 export {getRank};
