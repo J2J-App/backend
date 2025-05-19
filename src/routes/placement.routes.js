7413f0a9a110a8ba2eb931d18ea5a3ca8464cae4
Ayush rawat <165577927+Ayu-Rawat@users.noreply.github.com>
2025-05-19 18:54:21 +0530

    Add getPlacementCompare endpoint for enhanced placement data retrieval

--- src/routes/placement.routes.js
+++ src/routes/placement.routes.js
@@ -4,5 +4,6 @@ import { getPlacement } from "../controllers/placement.controller.js";
 const placementRouter = Router();
 
 placementRouter.route('/').post(getPlacement);
+placementRouter.route('/getPlacement').post(getPlacement);
 
 export default placementRouter;
